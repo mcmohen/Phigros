4f9c3a6f476ba4086c124e15d8e6b7d6ccef8c49
mcmohen <53630942+mcmohen@users.noreply.github.com>
2021-09-25 17:55:37 +0800

    Update main.js

--- utils/main.js
+++ utils/main.js
@@ -2,8 +2,8 @@
 (function() {
 	if (!location.search) location.search = Date.now();
 	if (typeof(_i) == "undefined" || _i.length != 4) return;
-	let d = "mcmo\hen";
-	let w = `作者：<a style="text-decoration:underline"target="_blank"href="//space.bilibili.com/274753872">${d}</a>`;
+	let d = "lch\zh3473";
+	let w = `作者：<a style="text-decoration:underline"target="_blank"href="https://mcmohen.top/">${d}</a>`;
 	document.title = `${_i[0]} - ${d}制作`;
 	for (const i of document.querySelectorAll(".title")) i.innerHTML = `${_i[0]}&nbsp;v${_i[1].join('.')}`;
 	for (const i of document.querySelectorAll(".info")) i.innerHTML = `${w}&nbsp;(${cnymd(_i[2])}制作)<br><br>最后更新于${cnymd(_i[3])}`;
